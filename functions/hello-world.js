fecd9075b9bdb045407a493e3ca6ace7ad563d12
Tim Claessen <timclaessen96@gmail.com>
2025-07-26 14:55:04 +0800

    Update hello-world.js

--- functions/hello-world.js
+++ functions/hello-world.js
@@ -2,6 +2,10 @@
 exports.handler = async (event, context) => {
   return {
     statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Headers": "Content-Type"
+    },
     body: JSON.stringify({ message: "Hello from Netlify Functions!" }),
   };
 };
